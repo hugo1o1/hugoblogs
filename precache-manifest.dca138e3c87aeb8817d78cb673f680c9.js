7e52ec715d544c91d4451d26c3903250c2c226f6
WitHugo <l>
2019-05-14 18:43:51 +0800

    Updates

---
+++ precache-manifest.dca138e3c87aeb8817d78cb673f680c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed0bbf830851e4164006f5a39c45015",
+    "url": "/hugoblogs/index.html"
+  },
+  {
+    "revision": "c334a509e4cc19c0501c",
+    "url": "/hugoblogs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ed3414b097ec5d5000be",
+    "url": "/hugoblogs/static/js/2.f7844318.chunk.js"
+  },
+  {
+    "revision": "c334a509e4cc19c0501c",
+    "url": "/hugoblogs/static/js/main.036b7da0.chunk.js"
+  },
+  {
+    "revision": "e41f4aa2c752e353843d",
+    "url": "/hugoblogs/static/js/runtime~main.01bc9d0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hugoblogs/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>万芷辉的个人博客即将上线</p>
+    </div>
+  );
+}
+
+export default App;
